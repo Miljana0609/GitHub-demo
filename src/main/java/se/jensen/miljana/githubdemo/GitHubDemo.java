62f5989973a11e27d55ae434011004fe2392dc4a
Miljana0609 <miljanazoranovic25@gmail.com>
2025-09-01 14:36:03 +0200

    commit-meddelande

--- src/main/java/se/jensen/miljana/githubdemo/GitHubDemo.java
+++ src/main/java/se/jensen/miljana/githubdemo/GitHubDemo.java
@@ -1,4 +1,8 @@
 package se.jensen.miljana.githubdemo;
 
 public class GitHubDemo {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+        System.out.println("WORLD");
+    }
 }
